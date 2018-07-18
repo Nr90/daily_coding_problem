328f4b4af33ff5042f94e12bb00e70617cc91e9e
Elbert Fliek <efliek@gmail.com>
2018-07-18 22:21:05 +0000

    switched from asserts to unittest in 5/solution

--- 5/solution.py
+++ 5/solution.py
@@ -1,3 +1,18 @@
+"""
+This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+"""
+import unittest
+
 
 def cons(a, b):
     def pair(f):
@@ -13,5 +28,13 @@ def cdr(pair):
     return pair(lambda x, y: y)
 
 
-assert car(cons(3, 4)) == 3
-assert cdr(cons(3, 4)) == 4
+class TestSolutions(unittest.TestCase):
+    def test_car(self: 'TestSolutions'):
+        self.assertEqual(car(cons(3, 4)), 3)
+
+    def test_cdr(self: 'TestSolutions'):
+        self.assertEqual(cdr(cons(3, 4)), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
