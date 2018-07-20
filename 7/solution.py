ad24b546efdc1109f42552f50ce9c1f1f00b0e9c
Elbert Fliek <efliek@gmail.com>
2018-07-20 18:14:41 +0000

    fixed special case where second number was 0

--- 7/solution.py
+++ 7/solution.py
@@ -15,6 +15,8 @@ def count_ways(message):
         return 1
     if int(message[0:2]) > 26:
         return count_ways(message[1:])
+    if message[1] == '0':
+        return count_ways(message[2:])
     return count_ways(message[1:]) + count_ways(message[2:])
 
 
@@ -22,6 +24,8 @@ class TestSolutions(unittest.TestCase):
     def test_count_ways(self: 'TestSolutions'):
         self.assertEqual(count_ways(''), 1)
         self.assertEqual(count_ways('1'), 1)
+        self.assertEqual(count_ways('10'), 1)
+        self.assertEqual(count_ways('101'), 1)
         self.assertEqual(count_ways('27'), 1)
         self.assertEqual(count_ways('111'), 3)
 
