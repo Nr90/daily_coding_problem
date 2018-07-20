06500429a2e1d0ce7a5b5b1a81cb39e6697909c7
Elbert Fliek <efliek@gmail.com>
2018-07-20 18:09:28 +0000

    added problem 7

---
+++ 7/solution.py
@@ -0,0 +1,30 @@
+"""
+This problem was asked by Facebook.
+
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+You can assume that the messages are decodable. For example, '001' is not allowed.
+"""
+import unittest
+
+
+def count_ways(message):
+    if len(message) == 0 or len(message) == 1:
+        return 1
+    if int(message[0:2]) > 26:
+        return count_ways(message[1:])
+    return count_ways(message[1:]) + count_ways(message[2:])
+
+
+class TestSolutions(unittest.TestCase):
+    def test_count_ways(self: 'TestSolutions'):
+        self.assertEqual(count_ways(''), 1)
+        self.assertEqual(count_ways('1'), 1)
+        self.assertEqual(count_ways('27'), 1)
+        self.assertEqual(count_ways('111'), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
