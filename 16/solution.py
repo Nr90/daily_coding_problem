7733a3e433fa5ce65082fe3c6e57f7ff5d840992
Elbert Fliek <efliek@gmail.com>
2018-07-30 21:42:35 +0000

    added problem 16

---
+++ 16/solution.py
@@ -0,0 +1,37 @@
+"""
+This problem was asked by Twitter.
+
+You run an e-commerce website and want to record the last N order ids in a log.
+Implement a data structure to accomplish this, with the following API:
+
+    record(order_id): adds the order_id to the log
+    get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
+
+You should be as efficient with time and space as possible.
+"""
+import unittest
+from typing import Callable, List, Tuple
+
+
+def get_logger() -> Tuple[Callable, Callable]:
+    log = []  # type: List[int]
+    def record(order_id: int) -> None:
+        log.append(order_id)
+    
+    def get_last(i: int) -> int:
+        return log[-i]
+    
+    return record, get_last
+
+
+
+class TestSolution(unittest.TestCase):
+    def test_small(self: 'TestSolution') -> None:
+        record, get_last = get_logger()
+        for i in range(5):
+            record(i)
+        self.assertEqual(get_last(1), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
