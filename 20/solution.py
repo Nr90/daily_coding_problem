80b3751b5601c9472a79e1071c51caaa9c78f80b
Elbert Fliek <efliek@gmail.com>
2018-08-04 20:19:15 +0000

    switched to simpler solution

--- 20/solution.py
+++ 20/solution.py
@@ -18,44 +18,38 @@ class Node:
         self.value = value
         self.next = next
 
-    def __iter__(self) -> Generator['Node', None, None]:
-        cur = self  # type: Optional['Node']
-        while cur is not None:
-            yield cur
-            cur = cur.next
 
-    def __len__(self) -> int:
-        return sum(1 for el in self)
-
-    def __next__(self) -> Optional['Node']:
-        return self.next
-
-
-def advance(it: Iterator, n: int) -> None:
-    for i in range(n):
-        next(it)
-
-
-def find_intersection_measure(A: Node, B: Node) -> Optional[Node]:
-    a_len = len(A)
-    b_len = len(B)
-    if a_len > b_len:
-        advance(iter(A), a_len - b_len)
-    else:
-        advance(iter(B), b_len - a_len)
-    for (a, b) in zip(A, B):
-        if a.value == b.value:
-            return a
-    return None
+def find_intersection(A: Node, B: Node) -> Optional[Node]:
+    if A is None or B is None:
+        return None
+    
+    a, b = A, B
+    while a.value != b.value:
+        if a == None:
+            a = B
+        else:
+            a = a.next
+        if b == None:
+            b = A
+        else:
+            b = b.next
+    return a
 
 
 class TestSolution(unittest.TestCase):
     def test_given(self) -> None:
         A = Node(3, Node(7, Node(8, Node(10))))
         B = Node(99, Node(1, Node(8, Node(10))))
-        inter = find_intersection_measure(A, B)
+        inter = find_intersection(A, B)
         assert inter
         self.assertEqual(inter.value, 8)
+
+    def test_alternative(self) -> None:
+        A = Node(3, Node(8, Node(7, Node(10))))
+        B = Node(99, Node(1, Node(8, Node(10))))
+        inter = find_intersection(A, B)
+        assert inter
+        self.assertEqual(inter.value, 10)
 
 
 if __name__ == '__main__':
