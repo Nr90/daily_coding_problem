d04665be5a0e9ccb9298b3e620fbc3e0fbadef69
Elbert Fliek <efliek@gmail.com>
2018-08-04 18:33:15 +0000

    added a template for the problems

---
+++ template/solution.py
@@ -0,0 +1,13 @@
+"""
+"""
+import unittest
+
+
+
+class TestSolution(unittest.TestCase):
+    def test_given(self) -> None:
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
