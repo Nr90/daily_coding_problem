5d346c66036cb62e8ecebd2b9cb01a00e4b4542a
Elbert Fliek <efliek@gmail.com>
2018-08-14 17:05:43 +0000

    added problem 31

---
+++ 31/solution.py
@@ -0,0 +1,23 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+The edit distance between two strings refers to the minimum
+number of character insertions, deletions, and substitutions
+required to change one string to the other. For example,
+the edit distance between “kitten” and “sitting” is three:
+substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+
+Given two strings, compute the edit distance between them.
+"""
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    def test_given(self) -> None:
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
