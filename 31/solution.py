baa62e1e5b3f4145a0e8ce91bc7f9a32d948e7ae
Elbert Fliek <efliek@gmail.com>
2018-08-16 16:42:13 +0000

    added solution for problem 31

--- 31/solution.py
+++ 31/solution.py
@@ -14,9 +14,20 @@ Given two strings, compute the edit distance between them.
 import unittest
 
 
+def levenshtein(s1: str, s2: str) -> int:
+    if not s1:
+        return len(s2)
+    if not s2:
+        return len(s1)
+    cost = 0 if s1[-1] == s2[-1] else 1
+    return min([levenshtein(s1[:-1], s2) + 1,
+               levenshtein(s1, s2[:-1]) + 1,
+               levenshtein(s1[:-1], s2[:-1]) + cost])
+
+
 class TestSolution(unittest.TestCase):
     def test_given(self) -> None:
-        self.assertEqual(True, True)
+        self.assertEqual(levenshtein('kitten', 'sitting'), 3)
 
 
 if __name__ == '__main__':
