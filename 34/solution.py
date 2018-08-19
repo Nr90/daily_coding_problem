0a67c5fe1dc165a47a4a41725c561c8c6cb883c9
Elbert Fliek <efliek@gmail.com>
2018-08-19 10:11:55 +0000

    added problem 34

---
+++ 34/solution.py
@@ -0,0 +1,33 @@
+"""
+This problem was asked by Quora.
+
+Given a string, find the palindrome that can be made by inserting
+the fewest number of characters as possible anywhere in the word.
+If there is more than one palindrome of minimum length that can be made,
+return the lexicographically earliest one (the first one alphabetically).
+
+For example, given the string "race", you should return "ecarace",
+since we can add three letters to it (which is the smallest amount
+to make a palindrome). There are seven other palindromes that can be made
+from "race" by adding three letters, but "ecarace" comes first alphabetically.
+
+As another example, given the string "google",
+you should return "elgoogle".
+"""
+import unittest
+
+
+def find_palindrome(s: str) -> str:
+    return s
+
+
+class TestSolution(unittest.TestCase):
+    def test_given_race(self) -> None:
+        self.assertEqual(find_palindrome('race'), 'ecarace')
+
+    def test_given_google(self) -> None:
+        self.assertEqual(find_palindrome('google'), 'elgoog')
+
+
+if __name__ == '__main__':
+    unittest.main()
