966417d8349f82866e7db4259e2d14f0c4ebb1ad
Elbert Fliek <efliek@gmail.com>
2018-08-19 13:17:28 +0000

    implemented a solution for 34

--- 34/solution.py
+++ 34/solution.py
@@ -15,18 +15,40 @@ As another example, given the string "google",
 you should return "elgoogle".
 """
 import unittest
+from typing import Set
+
+
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
 
 
 def find_palindrome(s: str) -> str:
-    return s
+    if is_palindrome(s):
+        return s
+    l = len(s)
+    while l > 0:
+        for i in range(0, len(s) - l):
+            if is_palindrome(s[i:i+l]):
+                return s[i+l:][::-1] + s[i:i+l] + s[i+l:]
+        l -= 1
+    raise Exception("Could not create palindrome")
 
 
 class TestSolution(unittest.TestCase):
+    def test_is_palindrome(self) -> None:
+        self.assertTrue(is_palindrome('elgoogle'))
+        self.assertFalse(is_palindrome('google'))
+        self.assertTrue(is_palindrome('ecarace'))
+        self.assertFalse(is_palindrome('race'))
+
+    def test_already_palindrome(self) -> None:
+        self.assertEqual(find_palindrome('ecarace'), 'ecarace')
+
     def test_given_race(self) -> None:
         self.assertEqual(find_palindrome('race'), 'ecarace')
 
     def test_given_google(self) -> None:
-        self.assertEqual(find_palindrome('google'), 'elgoog')
+        self.assertEqual(find_palindrome('google'), 'elgoogle')
 
 
 if __name__ == '__main__':
