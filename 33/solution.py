adb32ffc948340e06c72d00fd043b432cfdb7348
Elbert Fliek <efliek@gmail.com>
2018-08-19 13:54:36 +0000

    added a very simple solution/

--- 33/solution.py
+++ 33/solution.py
@@ -19,12 +19,35 @@ your algorithm should print out:
 2
 2
 """
+import sys
 import unittest
+from contextlib import contextmanager
+from io import StringIO
+from typing import List
+from statistics import median
+
+
+@contextmanager
+def captured_output():
+    new_out = StringIO()
+    old_out = sys.stdout
+    try:
+        sys.stdout = new_out
+        yield sys.stdout
+    finally:
+        sys.stdout = old_out
+
+
+def running_mean(seq: List[int]) -> None:
+    for i in range(1, len(seq) + 1):
+        print('%g' % median(seq[:i]))
 
 
 class TestSolution(unittest.TestCase):
     def test_given(self) -> None:
-        self.assertEqual(True, True)
+        with captured_output() as out:
+            running_mean([2, 1, 5, 7, 2, 0, 5])
+        self.assertEqual(out.getvalue(), '2\n1.5\n2\n3.5\n2\n2\n2\n')
 
 
 if __name__ == '__main__':
