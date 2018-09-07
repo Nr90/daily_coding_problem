60aca6dea3a3d98eebbb00742495a19e0fae9f52
Elbert Fliek <efliek@gmail.com>
2018-09-07 08:29:13 +0000

    implemented 49

--- 49/solution.py
+++ 49/solution.py
@@ -13,11 +13,24 @@ the maximum sum would be 0, since we would not take any elements.
 Do this in O(N) time.
 """
 import unittest
+from typing import List
+
+
+def max_sub_sum(arr: List[int]) -> int:
+    n = len(arr)
+    solution = [0] * n
+    solution[0] = arr[0]
+    for i in range(1, n):
+        solution[i] = max(solution[i-1]+arr[i], arr[i])
+    return max(solution + [0])
 
 
 class TestSolution(unittest.TestCase):
-    def test_given(self) -> None:
-        self.assertEqual(True, True)
+    def test_given_mixed(self) -> None:
+        self.assertEqual(max_sub_sum([34, -50, 42, 14, -5, 86]), 137)
+
+    def test_given_all_negative(self) -> None:
+        self.assertEqual(max_sub_sum([-5, -1, -8, -9]), 0)
 
 
 if __name__ == '__main__':
