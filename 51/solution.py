c25f6d65e79b632c74d2af610f1a51ce39818c1a
Elbert Fliek <efliek@gmail.com>
2018-09-07 09:20:55 +0000

    implemented unbiased shuffle using swaps

--- 51/solution.py
+++ 51/solution.py
@@ -11,11 +11,26 @@ It should run in O(N) time.
 Hint: Make sure each one of the 52! permutations of the deck is equally likely.
 """
 import unittest
+from random import randint
+from typing import List, Dict
+from collections import defaultdict
+
+
+def shuffle(deck: List[str]) -> List[str]:
+    n = len(deck)
+    for i in range(n):
+        rand_j = randint(i, n-1)
+        deck[i], deck[rand_j] = deck[rand_j], deck[i]
+    return deck
 
 
 class TestSolution(unittest.TestCase):
     def test_given(self) -> None:
-        self.assertEqual(True, True)
+        deck = ['Ahearts', 'Kclubs', 'Jdiamonds', '10spades']
+        perms = defaultdict(lambda: 0)  # type: Dict[str, int]
+        for _ in range(1000000):
+            perms[','.join(shuffle(deck))] += 1
+        self.assertTrue(max(perms.values()) - min(perms.values()) < 1000)
 
 
 if __name__ == '__main__':
