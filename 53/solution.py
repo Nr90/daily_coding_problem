3d3b04fa68ec888d23296b6aa78e9a2853e04e10
Elbert Fliek <efliek@gmail.com>
2018-09-07 09:31:03 +0000

    implemented queue using double stacks by creating heavy dequeue, light enqueue

--- 53/solution.py
+++ 53/solution.py
@@ -8,11 +8,33 @@ with the following methods:
     dequeue, which removes it.
 """
 import unittest
+from typing import Any, List
+
+
+class Queue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, elem: Any) -> None:
+        self.stack1.append(elem)
+
+    def dequeue(self) -> Any:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
 
 
 class TestSolution(unittest.TestCase):
-    def test_given(self) -> None:
-        self.assertEqual(True, True)
+    def test_queue(self) -> None:
+        q = Queue()
+        q.enqueue(1)
+        q.enqueue(2)
+        q.enqueue(3)
+        self.assertEqual(q.dequeue(), 1)
+        self.assertEqual(q.dequeue(), 2)
+        self.assertEqual(q.dequeue(), 3)
 
 
 if __name__ == '__main__':
