f64db35ff4077822ea8251095b7f5fdfe40bc5ca
Elbert Fliek <efliek@gmail.com>
2018-09-07 10:13:51 +0000

    generator creates substrings from long to short, they are checked for being a palindrome

--- 46/solution.py
+++ 46/solution.py
@@ -8,9 +8,24 @@ For example, the longest palindromic substring of "aabcdcb" is "bcdcb".
 The longest palindromic substring of "bananas" is "anana".
 """
 import unittest
+from typing import Iterator
+
+
+def gen_substrings(s: str) -> Iterator[str]:
+    n = len(s)
+    for length in range(n, 0, -1):
+        for start in range(n-length+1):
+            yield s[start:start+length]
+
+
+def check_palindrome(s: str) -> bool:
+    return s == s[::-1]
 
 
 def substring_palindrome(s: str) -> str:
+    for sub_s in gen_substrings(s):
+        if check_palindrome(sub_s):
+            return sub_s
     return ''
 
 
