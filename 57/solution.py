b204858eead668ef493dd903fd388695d5f1c57b
Elbert Fliek <efliek@gmail.com>
2018-09-10 08:49:17 +0000

    implemented a simple solution

--- 57/solution.py
+++ 57/solution.py
@@ -16,11 +16,37 @@ and k = 10, you should return:
 No string in the list has a length of more than 10.
 """
 import unittest
+from typing import List, Optional
+
+
+def break_up(s: str, k: int) -> Optional[List[str]]:
+    lines = []
+    while ' ' in s:
+        last_space = 0
+        for i in range(len(s)):
+            if i > k:
+                break
+            if s[i] == ' ':
+                last_space = i
+        if not last_space:
+            return None
+        lines.append(s[0:last_space])
+        s = s[last_space+1:]
+    if len(s) <= k:
+        lines.append(s)
+    else:
+        return None
+    return lines
 
 
 class TestSolution(unittest.TestCase):
     def test_given(self) -> None:
-        self.assertEqual(True, True)
+        s = 'the quick brown fox jumps over the lazy dog'
+        k = 10
+        self.assertEqual(
+            break_up(s, k),
+            ['the quick', 'brown fox', 'jumps over', 'the lazy', 'dog']
+        )
 
 
 if __name__ == '__main__':
