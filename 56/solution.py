46291715b0d3cb0032b802f2bbb1fdb476c96760
Elbert Fliek <efliek@gmail.com>
2018-09-10 07:45:34 +0000

    added problem 57

---
+++ 56/solution.py
@@ -0,0 +1,17 @@
+"""
+This problem was asked by Google.
+
+Given an undirected graph represented as an adjacency matrix and an integer k,
+write a function to determine whether each vertex in the graph can be colored
+such that no two adjacent vertices share the same color using at most k colors.
+"""
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    def test_given(self) -> None:
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
