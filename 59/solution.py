5c1f572c3f12b5e15dca9dfa2b0126c0a2adf752
Elbert Fliek <efliek@gmail.com>
2018-09-12 13:41:45 +0000

    added problem 59

---
+++ 59/solution.py
@@ -0,0 +1,17 @@
+"""
+This problem was asked by Google.
+
+Implement a file syncing algorithm for two computers
+over a low-bandwidth network.
+What if we know the files in the two computers are mostly the same?
+"""
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    def test_given(self) -> None:
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
