cc2b0f6ca45ddb07d1d72ada7d0c8b40a7a20cfe
Elbert Fliek <efliek@gmail.com>
2018-09-25 18:35:16 +0000

    implemented an iterative solution

--- p73/solution.jl
+++ p73/solution.jl
@@ -5,16 +5,36 @@ Given the head of a singly linked list, reverse it in-place.
 =#
 using Test
 
-struct LinkedListNode{T}
+
+mutable struct LinkedListNode{T}
     value::T
     next::Union{LinkedListNode{T},Nothing}
 end
 
-function reverse!(head::LinkedListNode)
+
+mutable struct LinkedList
+    head::LinkedListNode
 end
 
-head = LinkedListNode(1, LinkedListNode(2, LinkedListNode(3, nothing)))
-reverse!(head)
-@test head.value == 3
-@test head.next.value == 2
-@test head.next.next.value == 1
+
+function reverse!(list::LinkedList)
+    prev = nothing
+    current = list.head
+    next = current.next
+
+    while current != nothing
+        current.next = prev
+        prev = current
+        current = next
+        if next != nothing
+            next = next.next
+        end
+    end
+    list.head = prev
+end
+
+list = LinkedList(LinkedListNode(1, LinkedListNode(2, LinkedListNode(3, nothing))))
+reverse!(list)
+@test list.head.value == 3
+@test list.head.next.value == 2
+@test list.head.next.next.value == 1
