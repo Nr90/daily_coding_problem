af9c95ba93c2cf45ccdebc277787b5bb958352c7
Elbert Fliek <efliek@gmail.com>
2018-10-16 20:08:50 +0000

    removed unused imports

--- p78/src/main/java/com/nr90/dailycodingproblem/Solution.java
+++ p78/src/main/java/com/nr90/dailycodingproblem/Solution.java
@@ -1,6 +1,5 @@
 package com.nr90.dailycodingproblem;
 import java.util.LinkedList;
-import java.util.Iterator;
 
 /**
  * This problem was asked recently by Google.
--- p78/src/test/java/com/nr90/dailycodingproblem/SolutionTest.java
+++ p78/src/test/java/com/nr90/dailycodingproblem/SolutionTest.java
@@ -1,8 +1,6 @@
 package com.nr90.dailycodingproblem;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import java.util.LinkedList;
 
 /**
