56b8061b17d0b3c47b979f6f87f0f9bb3dc9a9f1
Elbert Fliek <efliek@gmail.com>
2019-03-31 19:56:16 +0000

    added solution to problem 86

---
+++ p86/solution.jl
@@ -0,0 +1,40 @@
+#=
+This problem was asked by Google.
+
+Given a string of parentheses,
+write a function to compute the minimum number of parentheses to be removed
+to make the string valid (i.e. each open parenthesis is eventually closed).
+
+For example, given the string "()())()", you should return 1.
+Given the string ")(", you should return 2, since we must remove all of them.
+=#
+using Test
+
+
+function validate(s)
+    close_without_open = 0
+    opens = 0
+    for c in s
+        if c == '('
+            opens += 1
+            continue
+        end
+        # c == ')'
+        if opens == 0
+            close_without_open += 1
+            continue
+        end
+        opens -= 1
+    end
+    return opens + close_without_open
+end
+
+
+function tests()
+    @test validate("()()()") == 0
+    @test validate("()())()") == 1
+    @test validate(")(") == 2
+end
+
+
+tests()
