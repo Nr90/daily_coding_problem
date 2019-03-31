5f2b31cb189cce0bfaed092e7d926c3b01f069b9
Elbert Fliek <efliek@gmail.com>
2019-03-31 22:05:53 +0000

    added problem 87

---
+++ p87/solution.jl
@@ -0,0 +1,36 @@
+#=
+This problem was asked by Uber.
+
+A rule looks like this:
+A NE B
+This means this means point A is located northeast of point B.
+
+A SW C
+means that point A is southwest of C.
+
+Given a list of rules, check if the sum of the rules validate. For example:
+
+A N B
+B NE C
+C N A
+does not validate, since A cannot be both north and south of C.
+
+A NW B
+A N B
+is considered valid.
+=#
+using Test
+
+
+function validate(rules::Array{String, 1})
+    return false
+end
+
+
+function tests()
+    @test validate(["A N B", "B NE C", "C N A"]) == false
+    @test validate(["A NW B", "A N B"]) == true
+end
+
+
+tests()
